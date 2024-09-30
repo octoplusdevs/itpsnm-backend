778f6e57e25da15f8863ea9cdcd4670642c23abb
danguya <daniel.yava16@gmail.com>
2024-09-30 14:03:28 +0100

    associando o usuario ao estudante ou funcionario v2

--- src/http/controllers/auth/register.ts
+++ src/http/controllers/auth/register.ts
@@ -8,12 +8,14 @@ export async function registerController(request: FastifyRequest, reply: Fastify
   // Definição do esquema de validação para o corpo da requisição
   const registerSchema = z.object({
     email: z.string().email(),
+    employeeId: z.number(),
+    studentId: z.number(),
     password: z.string().min(6),  // Definindo uma senha com no mínimo 6 caracteres
     role: z.enum(['STUDENT', 'ADMIN', 'TEACHER'])  // Adicionando um enum para o papel do usuário
   });
 
   // Validação dos dados da requisição
-  const { email, password, role } = registerSchema.parse(request.body);
+  const { email, password, role, employeeId, studentId } = registerSchema.parse(request.body);
 
   // Instanciação do repositório Prisma e do UseCase
   const usersRepository: UsersRepository = new PrismaUserRepository();
@@ -21,7 +23,7 @@ export async function registerController(request: FastifyRequest, reply: Fastify
 
   try {
     // Execução do UseCase
-    const result = await registerUseCase.execute({ email, password, role });
+    const result = await registerUseCase.execute({ email, password, role, employeeId, studentId });
     if (result.success) {
       // Retornar sucesso
       return reply.status(201).send({
