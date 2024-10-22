6caa7c99df7d6411946d34621b86d3829c8f411f
danguya <daniel.yava16@gmail.com>
2024-10-22 18:03:33 +0100

    approve payments

---
+++ src/http/controllers/payments/approve.ts
@@ -0,0 +1,41 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { PaymentNotFoundError } from '@/use-cases/errors/payment-not-found'
+import { PaymentIsNotPendingError } from '@/use-cases/errors/payment-is-not-pending'
+import { EmployeeNotFoundError } from '@/use-cases/errors/employee-not-found'
+import { makeApprovePaymentUseCase } from '@/use-cases/factories/make-approve-payment-use-case'
+
+export async function approvePayment(request: FastifyRequest, reply: FastifyReply) {
+  const approvePaymentSchema = z.object({
+    paymentId: z.number().int(),
+    employeeId: z.number().int(),
+  })
+
+  const { paymentId, employeeId } = approvePaymentSchema.parse(request.body)
+
+  try {
+    const approvePaymentUseCase = makeApprovePaymentUseCase()
+
+    const approvedPayment = await approvePaymentUseCase.execute({
+      paymentId,
+      employeeId,
+    })
+
+    return reply.status(200).send(approvedPayment)
+  } catch (err) {
+    if (err instanceof PaymentNotFoundError) {
+      return reply.status(404).send({ message: err.message })
+    }
+
+    if (err instanceof EmployeeNotFoundError) {
+      return reply.status(404).send({ message: err.message })
+    }
+
+    if (err instanceof PaymentIsNotPendingError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    console.error(err)
+    return reply.status(500).send({ message: 'Internal Server Error' })
+  }
+}
--- src/use-cases/payment/approve-payment.ts
+++ src/use-cases/payment/approve-payment.ts
@@ -2,6 +2,8 @@ import { PaymentRepository } from "@/repositories/payments-repository"
 import { PAY_STATUS } from "@prisma/client"
 import { PaymentNotFoundError } from "../errors/payment-not-found"
 import { PaymentIsNotPendingError } from "../errors/payment-is-not-pending"
+import { EmployeeRepository } from "@/repositories/employee-repository"
+import { EmployeeNotFoundError } from "../errors/employee-not-found"
 
 interface ApprovePaymentDTO {
   paymentId: number
@@ -9,7 +11,10 @@ interface ApprovePaymentDTO {
 }
 
 export class ApprovePaymentUseCase {
-  constructor(private paymentRepository: PaymentRepository) {}
+  constructor(
+    private paymentRepository: PaymentRepository,
+    private employeeRepository: EmployeeRepository,
+  ) {}
 
   async execute(data: ApprovePaymentDTO) {
     const payment = await this.paymentRepository.findPaymentById(data.paymentId)
@@ -17,7 +22,12 @@ export class ApprovePaymentUseCase {
       throw new PaymentNotFoundError()
     }
 
-    if (payment.status !== PAY_STATUS.PENDING) {
+    const employee = await this.employeeRepository.findById(data.employeeId)
+    if (!employee) {
+      throw new EmployeeNotFoundError()
+    }
+
+    if (payment.status !== PAY_STATUS.PENDING && payment.status !== PAY_STATUS.RECUSED) {
       throw new PaymentIsNotPendingError()
     }
 
