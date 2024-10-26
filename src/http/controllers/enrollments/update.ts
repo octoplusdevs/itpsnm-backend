f1d5cdd247d3bd100fce70944c8ee666b68f6292
danguya <daniel.yava16@gmail.com>
2024-10-26 19:19:56 +0100

    adicionando dividas ao estudante

--- src/http/controllers/enrollments/update.ts
+++ src/http/controllers/enrollments/update.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { z } from 'zod';
+import { date, z } from 'zod';
 import { makeUpdateEnrollmentUseCase } from '@/use-cases/factories/update-enrollment-use-case';
-import { EnrollementState } from '@prisma/client';
+import { EnrollementState, MonthName } from '@prisma/client';
 import { StudentNotFoundError } from '@/use-cases/errors/student-not-found';
 import { CourseNotFoundError } from '@/use-cases/errors/course-not-found';
 import { LevelNotFoundError } from '@/use-cases/errors/level-not-found';
@@ -9,6 +9,7 @@ import { EnrollmentNotFoundError } from '@/use-cases/errors/enrollment-not-found
 import { IdentityCardNumberNotExistsError } from '@/use-cases/errors/id-card-not-exists-error';
 import { IdentityCardNumberHasInUseExistsError } from '@/use-cases/errors/id-card-already-in-use-error';
 import { ClassNotExists } from '@/use-cases/errors/class-not-exists-error';
+import { makeCreateInvoiceUseCase } from '@/use-cases/factories/make-create-invoice-use-case';
 
 export async function update(request: FastifyRequest, reply: FastifyReply) {
   const createEnrollmentSchema = z.object({
@@ -18,6 +19,7 @@ export async function update(request: FastifyRequest, reply: FastifyReply) {
     docsState: z.nativeEnum(EnrollementState).optional(),
     paymentState: z.nativeEnum(EnrollementState).optional(),
     classeId: z.number().optional(),
+    employeeId: z.number(),
   });
 
 
@@ -30,6 +32,7 @@ export async function update(request: FastifyRequest, reply: FastifyReply) {
       paymentState,
       classeId,
       levelId,
+      employeeId,
     } = createEnrollmentSchema.parse(request.body);
 
     const updateEnrollmentUseCase = makeUpdateEnrollmentUseCase();
@@ -41,6 +44,27 @@ export async function update(request: FastifyRequest, reply: FastifyReply) {
       docsState,
       levelId,
       paymentState
+    });
+
+    let createInvoiceUseCase = makeCreateInvoiceUseCase()
+
+    let months = Object.values(MonthName)
+    let items = [];
+    for (let month of months) {
+      items.push({
+        description: "",
+        amount: 17000,
+        month,
+        qty: 1
+      })
+    }
+    await createInvoiceUseCase.execute({
+      enrollmentId: enrollment.enrollment.id,
+      employeeId,
+      dueDate: new Date(),
+      items,
+      issueDate: new Date(),
+      type: 'TUITION'
     });
 
     return reply.status(200).send(enrollment);
