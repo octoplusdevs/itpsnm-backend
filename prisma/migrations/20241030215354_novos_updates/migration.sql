e546a79477ec3a0987cdf4d4ffe4760d19718183
danguya <daniel.yava16@gmail.com>
2024-10-30 22:54:10 +0100

    sdsd

--- prisma/migrations/20241030215354_novos_updates/migration.sql
+++ prisma/migrations/20241030215354_novos_updates/migration.sql
@@ -2,8 +2,6 @@
   Warnings:
 
   - You are about to drop the column `course` on the `classes` table. All the data in the column will be lost.
-  - Added the required column `courseId` to the `classes` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `course_id` to the `classes` table without a default value. This is not possible if the table is not empty.
   - Changed the type of `name` on the `classes` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
   - Changed the type of `period` on the `classes` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
@@ -14,12 +12,16 @@ CREATE TYPE "PeriodType" AS ENUM ('MORNING', 'AFTERNOON', 'EVENING');
 -- CreateEnum
 CREATE TYPE "ClasseType" AS ENUM ('A', 'B', 'C', 'D');
 
+-- DropForeignKey
+ALTER TABLE "classes" DROP CONSTRAINT "classes_id_classroom_fkey";
+
 -- AlterTable
 ALTER TABLE "classes" DROP COLUMN "course",
-ADD COLUMN     "courseId" INTEGER NOT NULL,
-ADD COLUMN     "course_id" INTEGER NOT NULL,
+ADD COLUMN     "courseId" INTEGER,
+ADD COLUMN     "course_id" INTEGER,
 DROP COLUMN "name",
 ADD COLUMN     "name" "ClasseType" NOT NULL,
+ALTER COLUMN "id_classroom" DROP NOT NULL,
 DROP COLUMN "period",
 ADD COLUMN     "period" "PeriodType" NOT NULL;
 
@@ -27,4 +29,7 @@ ADD COLUMN     "period" "PeriodType" NOT NULL;
 ALTER TABLE "classrooms" ADD COLUMN     "capacity" INTEGER NOT NULL DEFAULT 40;
 
 -- AddForeignKey
-ALTER TABLE "classes" ADD CONSTRAINT "classes_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "classes" ADD CONSTRAINT "classes_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "courses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "classes" ADD CONSTRAINT "classes_id_classroom_fkey" FOREIGN KEY ("id_classroom") REFERENCES "classrooms"("id") ON DELETE SET NULL ON UPDATE CASCADE;
